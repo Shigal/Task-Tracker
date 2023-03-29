6627411c836d17e1d52bd5454b774c1d8bf55703
shigal <shiglaws@gmail.com>
2023-03-29 09:47:34 -0700

    added json-server

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task';
+
+export const TASKS: Task[]= [
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'Feb 6th at 6:20PM',
+        reminder: true
+    }, 
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 8th at 2:20PM',
+        reminder: true
+    }, 
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Feb 10th at 4:20PM',
+        reminder: true
+    }
+]; 
